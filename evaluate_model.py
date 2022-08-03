82d3c6600866c6884e62ef6b8a0946593c932277
Nikos Tsagkopoulos <n.tsagko@gmail.com>
2022-08-03 11:27:50 +0100

    disable evaluate_model.py

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,27 +1,23 @@
-import torch
-import torch as t
-from model import Siamese, load_model, get_custom_CNN, jit_load, get_parametrized_model
-from torch.utils.data import DataLoader
-from parser_grief import ImgPairDataset, CroppedImgPairDataset, ImgPairDatasetOld
-from torchvision.transforms import Resize
-from tqdm import tqdm
-from utils import get_shift, plot_samples, plot_displacement, affine
-import numpy as np
-from scipy import interpolate
-from configs import CONFIG
+from pathlib import Path
 
+import numpy as np
+import torch as t
+from scipy import interpolate
+from tqdm import tqdm
+
+from configs import CONFIG
+from model import get_parametrized_model
 
 device = t.device("cuda") if t.cuda.is_available() else t.device("cpu")
-# device = t.device("cpu")
 
-DATASET = "carlevaris"
+DATASET = "teach_and_repeat_2022_70cm"
 VISUALIZE = False
 PLOT_IMPORTANCES = False
-WIDTH = 512  # - 8
-CROP_SIZE = WIDTH - 8
-PAD = 31
+WIDTH = 640  # - 8
+CROP_SIZE = 248
+PAD = 15
 FRACTION = 8
-OUTPUT_SIZE = 64  #  WIDTH // FRACTION
+OUTPUT_SIZE = WIDTH // FRACTION  #  WIDTH // FRACTION
 CROPS_MULTIPLIER = 1
 BATCHING = CROPS_MULTIPLIER    # this improves evaluation speed by a lot
 MASK = t.zeros(OUTPUT_SIZE)
@@ -34,23 +30,17 @@ FILTER_SIZE = 3
 EMB_CHANNELS = 256
 RESIDUALS = 0
 
-EVAL_LIMIT = 2000
+EVAL_LIMIT = 1000
 TOLERANCE = 32
 
 MODEL_TYPE = "siam"
-MODEL = "model_eunord"
-# MODEL = "model_47"
+MODEL = "model_riseholme"
 
-
-transform = Resize(192)
-# transform = Resize(192 * 2)  # 4:3
-# transform = Resize((288, 512))
 crops_num = int((WIDTH // CROP_SIZE) * CROPS_MULTIPLIER)
 crops_idx = np.linspace(0, WIDTH-CROP_SIZE, crops_num, dtype=int) + FRACTION // 2
 
 # crops_idx = np.array([WIDTH // 2 - CROP_SIZE // 2])
 # crops_num = 1
-print(crops_num, np.array(crops_idx))
 histograms = np.zeros((1000, 64))
 
 
@@ -91,84 +81,61 @@ def get_histogram(src, tgt):
 def get_importance(src, tgt, displac):
     # displac here is in size of embedding (OUTPUT_SIZE)
     histogram = model(src, tgt, padding=PAD, displac=displac).cpu().numpy()
-    f = interpolate.interp1d(np.linspace(0, 512, OUTPUT_SIZE), histogram, kind="cubic")
+    f = interpolate.interp1d(np.linspace(0, 1000, OUTPUT_SIZE), histogram, kind="cubic")
     interpolated = f(np.arange(512))
     return interpolated[0]
 
 
-def eval_displacement(eval_model=None, path="./results_" + MODEL_TYPE + "/" + MODEL + ".pt", dataset_path=None):
-    global model
-    # backbone = get_custom_CNN(LAYER_POOL, FILTER_SIZE, EMB_CHANNELS)
-    # model = Siamese(backbone, padding=PAD, eb=END_BN).to(device)
-    # model = load_model(model, "./results_" + MODEL_TYPE + "/" + MODEL + ".pt")
-    if eval_model is not None:
-        model = eval_model
-    else:
-        model = load_model(model, path)
+# def eval_displacement(epoch, eval_data=None, eval_model=None, dir="./results/eval_displacement/"):
+#     def get_padding(output_shape):
+#         return int((output_shape - 2) / 2)
 
-    if dataset_path is not None:
-        dataset = ImgPairDataset(path=dataset_path, dataset=DATASET)
-    else:
-        dataset = ImgPairDataset(dataset=DATASET)
+#     global model
+#     if eval_model is not None:
+#         model = eval_model
+#     else:
+#         raise NotImplementedError
 
-    train_loader = DataLoader(dataset, 1, shuffle=False)
+#     if eval_data is None:
+#         raise NotImplementedError
 
-    model.eval()
-    with torch.no_grad():
-        abs_err = 0
-        valid = 0
-        idx = 0
-        errors = []
-        results = []
-        for batch in tqdm(train_loader):
-            source, target, displ = transform(batch[0].to(device)), transform(batch[1].to(device)), batch[2]
+#     pad = get_padding(WIDTH//FRACTION)
 
-            if abs(displ.numpy()[0]) > 256:
-                continue
+#     model.eval()
+#     with torch.no_grad():
+#         abs_err = 0
+#         valid = 0
+#         idx = 0
+#         errors = []
+#         results = []
+#         for batch in tqdm(eval_data):
+#             source, target, displ = batch[0].to(device), batch[1][..., FRACTION//2:-FRACTION//2].to(device), batch[2]
 
-            # do it in both directions target -> source and source -> target
-            histogram = get_histogram(source, target)
-            # shift_hist = get_shift(WIDTH, CROP_SIZE, histogram, crops_idx)
-            # histograms[idx, :] = histogram.cpu().numpy()
-            shift_hist = histogram.cpu()
-            # histogram = get_histogram(target, source)
-            # shift_hist += t.flip(get_shift(WIDTH, CROP_SIZE, histogram, crops_idx), dims=(-1, ))
-            f = interpolate.interp1d(np.linspace(0, 1008, OUTPUT_SIZE), shift_hist, kind="cubic")
-            interpolated = f(np.arange(1008))
-            # interpolated = np.interp(np.arange(0, 1024), np.linspace(0, 1024, OUTPUT_SIZE), shift_hist.numpy())
-            ret = -(np.argmax(interpolated) - 504)
-            # print(ret, displ)
-            results.append(ret)
-            displac_mult = 1024/WIDTH
-            tmp_err = (ret - displ.numpy()[0])/displac_mult
-            abs_err += abs(tmp_err)
-            errors.append(tmp_err)
-            if VISUALIZE and abs(ret - displ.numpy()[0]) >= TOLERANCE:
-                if PLOT_IMPORTANCES:
-                    importances = get_importance(source, target, int(ret/(FRACTION*displac_mult)))
-                else:
-                    importances = None
-                plot_displacement(source.squeeze(0).cpu(),
-                                  target.squeeze(0).cpu(),
-                                  shift_hist.squeeze(0).cpu(),
-                                  displacement=-displ.numpy()[0]/displac_mult,
-                                  importance=importances,
-                                  name=str(idx),
-                                  dir="results_" + MODEL_TYPE + "/eval_" + MODEL + "/")
-                print(ret, displ.numpy()[0])
-            if abs(ret - displ.numpy()[0]) < TOLERANCE:
-                valid += 1
-            idx += 1
+#             # do it in both directions target -> source and source -> target
+#             histogram = model(source.unsqueeze(0), target.unsqueeze(0), padding=pad)
+#             predicted_displ = np.argmax(histogram.cpu().numpy())
+            
+#             pixel_err = (predicted_displ - histogram.cpu().shape[-1] / 2) / (histogram.cpu().shape[-1] / 2) * WIDTH / 2
+#             tmp_err = (pixel_err - displ)
+#             abs_err += abs(tmp_err)
+#             results.append(pixel_err)
+#             errors.append(tmp_err)
 
-            if idx > EVAL_LIMIT:
-                break
+#             if abs(pixel_err - displ) < TOLERANCE:
+#                 valid += 1
+#             idx += 1
 
-        print("Evaluated:", "\nAbsolute mean error:", abs_err/idx, "\nPredictions in tolerance:", valid*100/idx, "%")
-        np.savetxt("results_" + MODEL_TYPE + "/eval_" + MODEL + "/" + DATASET + "_errors.csv", np.array(errors) * 2.0, delimiter=",")
-        # np.savetxt("results_" + MODEL_TYPE + "/eval_" + MODEL + "/" + DATASET + "_preds.csv", np.array(errors) * 2.0, delimiter=",")
-        # np.savetxt("results_" + MODEL_TYPE + "/eval_" + MODEL + "/" + DATASET + "_histograms.csv", histograms, delimiter=",")
-        return abs_err/idx, valid*100/idx
+#             if idx > EVAL_LIMIT:
+#                 break
+
+#         Path(f"{dir}").mkdir(parents=True, exist_ok=True)
+#         print("Evaluated:", "\nAbsolute mean error:", abs_err/idx, "\nPredictions in tolerance:", valid*100/idx, "%")
+#         np.savetxt(f"{dir}/eval_errors_{epoch}.csv", np.array(errors) * 2.0, delimiter=",")
+#         # np.savetxt("results_" + MODEL_TYPE + "/eval_" + MODEL + "/" + DATASET + "_preds.csv", np.array(errors) * 2.0, delimiter=",")
+#         # np.savetxt("results_" + MODEL_TYPE + "/eval_" + MODEL + "/" + DATASET + "_histograms.csv", histograms, delimiter=",")
+#         return abs_err/idx, valid*100/idx
 
 
 if __name__ == '__main__':
-    eval_displacement()
+    pass
+    #eval_displacement()
